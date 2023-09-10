ddf96eba0ffa202d8d4a34d00208f7cfb8a793ff
NirajPujari <pujari.niraj09@gmail.com>
2023-09-10 16:58:09 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Board from "./components/Board";
+function App() {
+  return (
+    <div className="app">
+      <Board />
+    </div>
+  );
+}
+
+export default App;
